77f91a2b9e13ad9fadc69b4df4d4fa18d4b12fa2
juanprrr <54974335+juanprrr@users.noreply.github.com>
2022-03-17 01:04:55 -0600

    [UPDATE] Modular version of components
    Now every component is located in src/app/Components

--- tabas-admin/src/app/app.module.ts
+++ tabas-admin/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { LoginComponent } from './Components/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
