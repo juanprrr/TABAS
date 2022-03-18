d2f96a2409d588f7e45aae0f3b2dbecc515e94b2
igmochang <ig.morales@estudiantec.cr>
2022-03-17 22:45:06 -0600

    [Update] Button route
    Se asocia el boton de iniciar sesion para redirigir a otra pagina

--- tabas-admin/src/app/Components/login/login.component.ts
+++ tabas-admin/src/app/Components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
-
   ngOnInit(): void {
   }
 
+  constructor(private router: Router){
+  }
+
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
+  }
+  
 }
+
