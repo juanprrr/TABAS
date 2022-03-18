1e5fb59d21368737348881c32f1963df267708a3
juanprrr <54974335+juanprrr@users.noreply.github.com>
2022-03-17 23:27:42 -0600

    [UPDATE] Sign in component completed and routed from login window

--- tabas-admin/src/app/app-routing.module.ts
+++ tabas-admin/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {WregisterComponent} from "../app/Components/wregister/wregister.component";
 import { LoginComponent } from './Components/login/login.component';
+import { SigninComponent } from './Components/signin/signin.component';
 
 const routes: Routes = [
   {path:'Wregister', component: WregisterComponent},
-  {path:'Login', component: LoginComponent}
+  {path:'Login', component: LoginComponent},
+  {path:'Signin', component: SigninComponent}
 ];
 
 @NgModule({
