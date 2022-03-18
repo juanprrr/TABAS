6c6ee88706257e62e94136d96b699b8780834ffe
juanprrr <54974335+juanprrr@users.noreply.github.com>
2022-03-17 22:08:30 -0600

    [CREATED] Sign in module

--- tabas-admin/src/app/app.module.ts
+++ tabas-admin/src/app/app.module.ts
@@ -6,13 +6,17 @@ import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { LoginComponent } from './Components/login/login.component';
 import { HomeComponent } from './Components/home/home.component';
+import { SigninComponent } from './Components/signin/signin.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    SigninComponent
+
   ],
   imports: [
     BrowserModule,
