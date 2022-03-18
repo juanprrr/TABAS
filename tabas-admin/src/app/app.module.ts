4aedabe2dc99a73b14d98145b879a86a45763069
igmochang <ig.morales@estudiantec.cr>
2022-03-17 22:34:02 -0600

    [update] Routing
    Primer paso hacia routing

--- tabas-admin/src/app/app.module.ts
+++ tabas-admin/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
 import { LoginComponent } from './Components/login/login.component';
@@ -15,8 +15,7 @@ import { SigninComponent } from './Components/signin/signin.component';
     NavBarComponent,
     LoginComponent,
     HomeComponent,
-    SigninComponent
-
+    routingComponents
   ],
   imports: [
     BrowserModule,
